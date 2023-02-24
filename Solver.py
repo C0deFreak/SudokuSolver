c1a12e8023cebe50217b9a4649842d11e9167cab
Code Freak <grgurinluka@gmail.com>
2023-02-24 20:37:22 +0100

    Simple Missing Number Finder
    This is the first part in making the program go through all possible combinations while solving the sudoku board. In this part I made a simple program that goes through the first column of the board and findes the missing numbers. Also I started the part of the program that makes all of the missing numbre combinations.

--- Solver.py
+++ Solver.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 column_full = []
 row_full = []
 box_full = []
@@ -8,13 +10,20 @@ one_box = []
 column_num = 9
 multi = 0
 
-#Puts your input in colums
+# Missing Numbers
+all_num_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+anl_clone = all_num_list
+unwanted = []
+
+
+# Puts your input in columns
 for column in range(column_num):
     column_input = input()
     column_list = column_input.split()
     column_full.append(column_list)
 
-#Converts columns in rows
+
+# Converts columns in rows
 for column_choice in range(column_num):
     one_row.clear()
     for first_index_row in range(column_num):
@@ -23,7 +32,7 @@ for column_choice in range(column_num):
     row_full.append(one_row.copy())
 
 
-#Converts columns in boxes
+# Converts columns in boxes
 for box_columns in range(9):
     one_box.clear()
     for box in range(3):
@@ -39,7 +48,27 @@ for box_columns in range(9):
         multi = 6
 
 
+for search_for_num in range(len(all_num_list)):
+    if anl_clone[search_for_num] in column_full[0]:
+        unwanted.append(anl_clone[search_for_num])
+
+for destroy in range(len(unwanted)):
+    anl_clone.remove(unwanted[destroy])
+
+print(anl_clone)
 
 
+# ˇˇA sudoku example ˇˇ
+"""
+3 X X 8 X 1 X X 2
+2 X 1 X 3 X 6 X 4
+X X X 2 X 4 X X X
+8 X 9 X X X 1 X 6
+X 6 X X X X X 5 X
+7 X 2 X X X 4 X 9
+X X X 5 X 9 X X X
+9 X 4 X 8 X 7 X 5
+6 X X 1 X 7 X X 3
+"""
 
 
